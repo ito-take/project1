41d4a59dd46f94de8dfc7800258318d314a80077
ito-take <nov09062530@icloud.com>
2026-01-03 10:20:40 +0900

    update tool_operator.py

--- tool_operator.py
+++ tool_operator.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import re
 import subprocess
@@ -6,18 +7,46 @@ import tomllib
 
 BASE_PATH = os.getcwd()
 TOML_PATH = 'config/config.toml'
+WORKFLOWS_PATH = '.github/workflows'
+SCRIPT_PATH = 'script'
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-t", "--target",
+        required=False
+    )
+
+    parser.add_argument(
+        "-m", "--mode",
+        required=False
+    )
+
+    parser.add_argument(
+        "-v", "--version",
+        required=False
+    )
+
+    parser.add_argument(
+        "-wt", "--workflow_target",
+        required=False
+    )
+
+    return parser.parse_args()
 
 def toml_decorder():
-  toml_path = os.path.join(BASE_PATH, os.path.normpath(TOML_PATH))
+    toml_path = os.path.join(BASE_PATH, os.path.normpath(TOML_PATH))
 
-  with open(toml_path, "rb") as f:
+    with open(toml_path, "rb") as f:
         return tomllib.load(f)
 
 def main():
+    args = parse_args()
     toml = toml_decorder()
-    print(toml)
-
-
+    
+    
+    
+    
 if __name__ == "__main__":
     main()
